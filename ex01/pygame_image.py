190a54e18f7da811db6791bcc48c4ebb08cc41cc
YuukiMachida <c0b2213430@edu.teu.ac.jp>
2023-06-13 15:46:37 +0900

    練習６

--- ex01/pygame_image.py
+++ ex01/pygame_image.py
@@ -13,15 +13,22 @@ def main():
 
 
     tmr = 0
+    x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [1600 - x, 0])
         screen.blit(kk_imglst[tmr % 2],[300,200])
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        tmr += 1
+        x += 1
+        if x > 1600 :
+            x = 0
+        
+
+        clock.tick(100)
 
 
 if __name__ == "__main__":
